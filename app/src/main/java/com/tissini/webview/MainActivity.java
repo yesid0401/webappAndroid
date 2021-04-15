086df7111e072bade507fa9331f2f3a1950bd07e
Yesid Sanchez <yesids@tissini.com>
2021-04-15 18:18:20 -0500

    fixed file upload problem in feedback

--- app/src/main/java/com/tissini/webview/MainActivity.java
+++ app/src/main/java/com/tissini/webview/MainActivity.java
@@ -6,6 +6,7 @@ import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
@@ -15,6 +16,9 @@ import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.JsResult;
+import android.webkit.ValueCallback;
+import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.widget.ProgressBar;
@@ -38,6 +42,11 @@ public class MainActivity extends AppCompatActivity {
     private ProgressBar progressBar;
     private SwipeRefreshLayout swipeRefreshLayout;
 
+    private ValueCallback<Uri> mUploadMessage;
+    public ValueCallback<Uri[]> uploadMessage;
+    public static final int REQUEST_SELECT_FILE = 100;
+    private final static int FILECHOOSER_RESULTCODE = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -56,14 +65,11 @@ public class MainActivity extends AppCompatActivity {
         webView            = (WebView) findViewById(R.id.webview);
 
         PushNotifications.start(getApplicationContext(), getString(R.string.instanceId));
- //       PushNotifications.addDeviceInterest("general");
- //       PushNotifications.stop();
-//        PushNotifications.removeDeviceInterest("general");
-//        PushNotifications.clearDeviceInterests();
+
         progressBar.setVisibility(View.INVISIBLE);
         getSupportActionBar().hide();
 
-        webView.setWebChromeClient(new MywebChromeClient());
+        webView.setWebChromeClient(new MyWebChromClientClass());
         webView.setWebViewClient(new MywebViewClient(this.progressBar,this.webView,this,getIntent()));
         webView.addJavascriptInterface(new WebAppInterface(this), "Webview");
         loadWebview();
@@ -96,6 +102,27 @@ public class MainActivity extends AppCompatActivity {
             super.onBackPressed();
 
         }
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            if (requestCode == REQUEST_SELECT_FILE) {
+                if (uploadMessage == null)
+                    return;
+                uploadMessage.onReceiveValue(WebChromeClient.FileChooserParams.parseResult(resultCode, intent));
+                uploadMessage = null;
+            }
+        } else if (requestCode == FILECHOOSER_RESULTCODE) {
+            if (null == mUploadMessage)
+                return;
+
+            Uri result = intent == null || resultCode != MainActivity.RESULT_OK ? null : intent.getData();
+            mUploadMessage.onReceiveValue(result);
+            mUploadMessage = null;
+        }
+
+
     }
 
     public  void onRefresh(){
@@ -179,4 +206,70 @@ public class MainActivity extends AppCompatActivity {
         NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(getApplicationContext());
         notificationManagerCompat.notify(NOTIFICATION_ID,builder.build());
     }
+
+
+    public class MyWebChromClientClass extends WebChromeClient {
+
+        @Override
+        public boolean onJsAlert(WebView view, String url, String message, JsResult result) {
+            return super.onJsAlert(view, url, message, result);
+        }
+        // For 3.0+ Devices (Start)
+        // onActivityResult attached before constructor
+        protected void openFileChooser(ValueCallback uploadMsg, String acceptType)
+        {
+            mUploadMessage = uploadMsg;
+            Intent i = new Intent(Intent.ACTION_GET_CONTENT);
+            i.addCategory(Intent.CATEGORY_OPENABLE);
+            i.setType("image/*");
+            startActivityForResult(Intent.createChooser(i, "File Browser"), FILECHOOSER_RESULTCODE);
+        }
+
+
+        // For Lollipop 5.0+ Devices
+        public boolean onShowFileChooser(WebView mWebView, ValueCallback<Uri[]> filePathCallback, WebChromeClient.FileChooserParams fileChooserParams)
+        {
+            if (uploadMessage != null) {
+                uploadMessage.onReceiveValue(null);
+                uploadMessage = null;
+            }
+
+            uploadMessage = filePathCallback;
+
+            Intent intent = null;
+            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+                intent = fileChooserParams.createIntent();
+            }
+            try
+            {
+                startActivityForResult(intent, REQUEST_SELECT_FILE);
+            } catch (ActivityNotFoundException e)
+            {
+                uploadMessage = null;
+                return false;
+            }
+            return true;
+        }
+
+        //For Android 4.1 only
+        protected void openFileChooser(ValueCallback<Uri> uploadMsg, String acceptType, String capture)
+        {
+            mUploadMessage = uploadMsg;
+            Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+            intent.addCategory(Intent.CATEGORY_OPENABLE);
+            intent.setType("image/*");
+            startActivityForResult(Intent.createChooser(intent, "File Browser"), FILECHOOSER_RESULTCODE);
+        }
+
+        protected void openFileChooser(ValueCallback<Uri> uploadMsg)
+        {
+            mUploadMessage = uploadMsg;
+            Intent i = new Intent(Intent.ACTION_GET_CONTENT);
+            i.addCategory(Intent.CATEGORY_OPENABLE);
+            i.setType("image/*");
+            startActivityForResult(Intent.createChooser(i, "File Chooser"), FILECHOOSER_RESULTCODE);
+        }
+    }
+
+
 }
--- app/src/main/java/com/tissini/webview/models/MywebChromeClient.java
+++ app/src/main/java/com/tissini/webview/models/MywebChromeClient.java
@@ -4,12 +4,13 @@ import android.webkit.JsResult;
 import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 
-public class MywebChromeClient extends WebChromeClient {
+public class MywebChromeClient extends WebChromeClient  {
 
     @Override
     public boolean onJsAlert(WebView view, String url, String message, JsResult result) {
         return super.onJsAlert(view, url, message, result);
     }
+
 
 }
 
