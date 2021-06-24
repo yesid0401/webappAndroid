5afbc1f9a56da1af2c25c1c182f2696c27591170
yesid sanchez <yesids@tissini.com>
2021-06-24 12:09:11 -0500

    add class SMSBroadcastReceiver

---
+++ app/src/main/java/com/tissini/webview/services/SMSBroadcastReceiver.java
@@ -0,0 +1,37 @@
+package com.tissini.webview.services;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.phone.SmsRetriever;
+import com.google.android.gms.common.api.CommonStatusCodes;
+import com.google.android.gms.common.api.Status;
+
+public class SMSBroadcastReceiver  extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (SmsRetriever.SMS_RETRIEVED_ACTION.equals(intent.getAction())) {
+            Bundle extras = intent.getExtras();
+            Status status = (Status) extras.get(SmsRetriever.EXTRA_STATUS);
+
+            switch(status.getStatusCode()) {
+                case CommonStatusCodes.SUCCESS:
+                    // Get SMS message contents
+                    String message = (String) extras.get(SmsRetriever.EXTRA_SMS_MESSAGE);
+                    // Extract one-time code from the message and complete verification
+                    // by sending the code back to your server.
+
+                    Toast.makeText(context, message, Toast.LENGTH_LONG).show();
+                    break;
+                case CommonStatusCodes.TIMEOUT:
+                    // Waiting for SMS timed out (5 minutes)
+                    // Handle the error ...
+                    break;
+            }
+        }
+    }
+}
